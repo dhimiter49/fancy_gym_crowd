6b15ff0b1b4c9d78659faac7e47a40e609c2ca59
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2024-05-30 21:31:54 +0200

    logic error

--- fancy_gym/envs/classic_control/crowd_navigation/crowd_navigation.py
+++ fancy_gym/envs/classic_control/crowd_navigation/crowd_navigation.py
@@ -249,7 +249,7 @@ class CrowdNavigationEnv(BaseCrowdNavigationEnv):
                     t_acc * self.MAX_ACC - max_step_acc, 0 - 1e-8, -max_step_acc
                 )
                 vels = np.concatenate([acc_vels, dec_vels])
-            if vels == []:
+            if len(vels) == 0:
                 vels = np.array([0])
 
             # Fix direction
