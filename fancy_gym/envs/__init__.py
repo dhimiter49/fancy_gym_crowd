5d5389ee1a525a45404e1a5a403b82d7a8962eee
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2024-06-17 10:40:36 +0200

    add more options for const vel in corridor

--- fancy_gym/envs/__init__.py
+++ fancy_gym/envs/__init__.py
@@ -77,9 +77,56 @@ register(
     kwargs={
         "n_crowd": 6,
         "width": 20,
-        "height": 6,
+        "height": 8,
         "interceptor_percentage": 2,
         "const_vel": True,
+    }
+)
+
+register(
+    id='fancy/CrowdNavigationConstVel-v0',
+    entry_point=CrowdNavigationEnv,
+    mp_wrapper=MPWrapper_Crowd,
+    max_episode_steps=100,
+    kwargs={
+        "n_crowd": 6,
+        "width": 20,
+        "height": 8,
+        "interceptor_percentage": 2,
+        "const_vel": True,
+        "velocity_control": True,
+    }
+)
+
+register(
+    id='fancy/CrowdNavigationConstLiDAR-v0',
+    entry_point=CrowdNavigationEnv,
+    mp_wrapper=MPWrapper_Crowd,
+    max_episode_steps=100,
+    kwargs={
+        "n_crowd": 6,
+        "width": 20,
+        "height": 8,
+        "interceptor_percentage": 2,
+        "const_vel": True,
+        "lidar_rays": 40,
+    }
+)
+
+register(
+    id='fancy/CrowdNavigationConstLiDARPolarVel-v0',
+    entry_point=CrowdNavigationEnv,
+    mp_wrapper=MPWrapper_Crowd,
+    max_episode_steps=100,
+    kwargs={
+        "n_crowd": 6,
+        "width": 20,
+        "height": 8,
+        "interceptor_percentage": 2,
+        "const_vel": True,
+        "lidar_rays": 40,
+        "polar": True,
+        "velocity_control": True,
     }
 )
 
