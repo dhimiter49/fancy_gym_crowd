b7f35568a92972cc46c5f0c8c961337ac75bd029
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2024-10-31 11:08:23 +0100

    small distance condition for dynamic env

--- fancy_gym/envs/classic_control/crowd_navigation/mp_wrapper.py
+++ fancy_gym/envs/classic_control/crowd_navigation/mp_wrapper.py
@@ -67,8 +67,8 @@ class MPWrapper_Crowd(RawInterfaceWrapper):
                 'max_vel': 3.0,
                 'horizon': 21,  # 2 sec to stop (1 extra step is current step)
                 'dt': 0.1,
-                'min_dist_crowd': 1.4,  # personal space of the members of the crowd
-                'min_dist_wall': 0.5,  # physical space of agent + 0.1
+                'min_dist_crowd': 0.8001,  # personal space of the members of the crowd
+                'min_dist_wall': 0.41,  # physical space of agent + 0.1
             },
             'trajectory_generator_kwargs': {
                 'weights_scale': 1,
@@ -118,8 +118,8 @@ class MPWrapper_Crowd_Vel(MPWrapper_Crowd):
                 'horizon': 21,  # 2 sec to stop (1 extra step is current step)
                 'dt': 0.1,
                 'velocity_control': True,
-                'min_dist_crowd': 1.4,  # personal space of the members of the crowd
-                'min_dist_wall': 0.5,  # physical space of agent + 0.1
+                'min_dist_crowd': 0.8001,  # personal space of the members of the crowd
+                'min_dist_wall': 0.41,  # physical space of agent + 0.1
             },
             'trajectory_generator_kwargs': {
                 'weights_scale': 1,
