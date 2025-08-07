7ba53a65643208a82a6fd82ca4159d5ab34f301a
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2025-08-07 15:17:29 +0200

    put same curriculum otherwise it doesn not work

--- fancy_gym/envs/classic_control/crowd_navigation/crowd_navigation_orca.py
+++ fancy_gym/envs/classic_control/crowd_navigation/crowd_navigation_orca.py
@@ -41,7 +41,7 @@ class CrowdNavigationORCAEnv(CrowdNavigationEnv):
         n_frames: int = 4,
         lidar_max: float = 0.0,
         intrinsic_rew: bool = False,
-        curriculum: Callable = lambda _: 8,
+        curriculum: Callable = lambda _: 4,
         one_goal: bool = False,
     ):
         super().__init__(
