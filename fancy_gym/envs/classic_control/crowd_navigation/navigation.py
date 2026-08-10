431030a89a415a50de8ed5bccccff84058e205b9
Dhimiter Pikuli <dhimiter.pikuli@inria.fr>
2026-08-10 12:17:03 +0200

    ttg for navigation as well

--- fancy_gym/envs/classic_control/crowd_navigation/navigation.py
+++ fancy_gym/envs/classic_control/crowd_navigation/navigation.py
@@ -236,6 +236,8 @@ class NavigationEnv(BaseCrowdNavigationEnv):
         self.update_state(action)
         self.exec_traj.append(self._agent_pos)
         self._goal_reached = self.check_goal_reached()
+        if self._goal_reached:
+            self.ttg()
         self._is_collided = self._check_collisions()
         self._current_reward, info = self._get_reward(action)
 
